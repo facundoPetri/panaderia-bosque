a4cb69c256afd6c3263ef1f71d3d819380fa0662
NicoGRodriguez <nicolas_rodriguez_94@mi.unc.edu.ar>
2024-08-10 18:12:52 -0300

    #72 feature se agrego modal

--- src/interfaces/Supplies.ts
+++ src/interfaces/Supplies.ts
@@ -1,16 +1,17 @@
 export interface SuppliesResponse {
-    _id: string
-    name:string,
-    description:string,
-    min_stock:number,
-    max_stock:number,
-    unit:string
-    size:string
-    createdAt:string
-    updatedAt:string
-  }
-  export interface SuppliesCreateData extends Omit<SuppliesResponse, '_id' | 'size'>  {
-    image:string
-    size:number
-  }
-  
+  _id: string
+  name: string
+  description: string
+  min_stock: number
+  max_stock: number
+  unit: string
+  size: string
+  createdAt: string
+  updatedAt: string
+  deletedAt?: string
+}
+export interface SuppliesCreateData
+  extends Omit<SuppliesResponse, '_id' | 'size'> {
+  image: string
+  size: number
+}
--- src/pages/Supplies/ExpiringSupply.tsx
+++ src/pages/Supplies/ExpiringSupply.tsx
@@ -1,108 +1,90 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GenericTable from '../../components/GenericTable';
 import { Column } from '../../components/GenericTable';
-  
-  const columns: Column<ExpiringSupplyI>[] = [
-    { id: 'id', label: 'id' , hiddenColumn: true, sortable : false},
-    { id: 'expirationDate', label: 'Fecha de vencimiento' },
-    { id: 'name', label: 'Nombre' },
-    { id: 'batchNumber', label: 'Numero de lote' },
-    { id: 'location', label: 'Ubicacion' },
-    { id: 'quantity', label: 'Cantidad' },
-  ];
+import { SuppliesResponse } from '../../interfaces/Supplies';
+import { request } from '../../common/request';
+import { formatDate } from '../../utils/dateUtils';
+import SuppliesDialogEdit from './SuppliesDialogEdit';
 
-interface ExpiringSupplyI {
-  id: string;
-  expirationDate: string;
-  name: string;
-  batchNumber: number;
-  location: string;
-  quantity: string;
-}
+const columns: Column<SuppliesResponse>[] = [
+  {
+    id: '_id',
+    label: 'id',
+    hiddenColumn: true,
+    sortable: false,
+    hiddenFilter: true,
+  },
+  { id: 'deletedAt', label: 'Fecha de vencimiento' },
+  { id: 'name', label: 'Nombre' },
+  { id: 'max_stock', label: 'Numero de lote' },
+  { id: 'description', label: 'Ubicacion' },
+  { id: 'unit', label: 'Cantidad' },
+];
+//TODO: VER bien por que no esta en el back
+const dropdownOptions = columns.map(column => ({
+  title: column.label,
+}));
 
-  const data: ExpiringSupplyI[] = [
-    {
-      id: '1',
-      expirationDate: '20/11/2023',
-      name: 'Azucar blanca',
-      batchNumber: 15,
-      location: 'Fila 1 columna 2',
-      quantity: '35 kg',
-    },
-    {
-      id: '2',
-      expirationDate: '21/11/2023',
-      name: 'Harina 000',
-      batchNumber: 3,
-      location: 'Fila 3 columna 1',
-      quantity: '35 kg',
-    },
-    {
-      id: '3',
-      expirationDate: '10/12/2023',
-      name: 'Huevo',
-      batchNumber: 3,
-      location: 'Fila 1 columna 1',
-      quantity: '100 u',
-    },
-    {
-      id: '4',
-      expirationDate: '16/12/2023',
-      name: 'Levadura en polvo',
-      batchNumber: 5,
-      location: 'Fila 2 columna 1',
-      quantity: '20 u',
-    },
-    {
-      id: '5',
-      expirationDate: '20/12/2023',
-      name: 'Margarina',
-      batchNumber: 10,
-      location: 'Fila 3 columna 3',
-      quantity: '50 kg',
-    },
-    {
-      id: '6',
-      expirationDate: '28/12/2023',
-      name: 'Sal',
-      batchNumber: 10,
-      location: 'Fila 2 columna 2',
-      quantity: '40 kg',
-    },
-  ];  
-
-  const dropdownOptions = columns.map(column => ({
-    title: column.label,
-  }));
-  
-  export default function ExpiringSupply() {
-    const [selectedSupplies, setSelectedSupplies] = useState<ExpiringSupplyI | null>(null);
+export default function ExpiringSupply() {
+  const [selectedSupplies, setSelectedSupplies] =
+    useState<SuppliesResponse | null>(null)
+    const [supplies, setSupplies] = useState<SuppliesResponse[]>([])
+    const [isEditMode, setIsEditMode] = useState<boolean>(false)
   //Modal
-    const onView = (expiringSupply: ExpiringSupplyI) => {
-        setSelectedSupplies(expiringSupply);
-    };
-  
-    const onClose = () => {
-        setSelectedSupplies(null);
-    };
-
-    const onDelete = (id: string) => {
-      console.log(`Eliminando elemento con id: ${id}`);
-      // Aquí puedes llamar a tu servicio de eliminación con el id
-    };
-  
-    return (
-      <div style={{ padding: '20px' }}>
-        <h1>Insumos con vencimiento próximo</h1>
-        <GenericTable
-          columns={columns}
-          data={data}
-          dropdownOptions={dropdownOptions} // Agrege dropdownOptions
-          onView={onView}
-          onDelete={onDelete}
-          showDropdown={false}
-          nameColumnId="name"
-        />
-      </div>
-    );
+  const onView = (supplies: SuppliesResponse) => {
+    setSelectedSupplies(supplies)
+    setIsEditMode(false)
   }
+
+  const onClose = () => {
+    setSelectedSupplies(null)
+    setIsEditMode(false)
+  }
+
+  const onDelete = (id: string) => {
+    console.log(`Eliminando elemento con id: ${id}`);
+    // Aquí puedes llamar a tu servicio de eliminación con el id
+  };
+
+  const getSupplies = async () => {
+    try {
+      const res = await request<SuppliesResponse[]>({
+        path: '/supplies',
+        method: 'GET',
+      })
+      if (res) {
+        const formattedSupplies = res.map((supplie) => ({
+          ...supplie,
+          updatedAt: formatDate(supplie.updatedAt),
+        }))
+        setSupplies(formattedSupplies)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getSupplies()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Insumos con vencimiento próximo</h1>
+      <GenericTable
+        columns={columns}
+        data={supplies}
+        dropdownOptions={dropdownOptions} // Agrege dropdownOptions
+        onView={onView}
+        onDelete={onDelete}
+        showDropdown={false}
+        nameColumnId="name"
+      />
+      <SuppliesDialogEdit
+        selectedSupplies={selectedSupplies}
+        onClose={onClose}
+        editable={isEditMode}
+      />
+    </div>
+  );
+}
