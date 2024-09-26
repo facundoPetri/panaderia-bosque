03e261bd72a7fb4748d72983f9ee703f3e47a77e
Facundo Petri <facundo.petri@intive.com>
2024-09-26 16:55:31 -0300

    Refactor DownloadPdfButton component to add tooltip for downloading PDF

--- src/components/DownloadPdfButton.tsx
+++ src/components/DownloadPdfButton.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import { downloadPdf } from '../utils/utils'
-import { Button, makeStyles } from '@material-ui/core'
+import { Button, Tooltip, makeStyles } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
 
 const useStyles = makeStyles(() => ({
   btnContainer: {
+    margin: '1rem .5rem',
     display: 'flex',
     justifyContent: 'flex-end',
   },
-}));
+}))
 
 const DownloadPdfButton = ({ url }: { url: string }) => {
   const classes = useStyles()
 
   return (
     <div className={classes.btnContainer}>
-      <Button onClick={() => downloadPdf(url)}>
-        <FontAwesomeIcon icon={faFilePdf} size="2x" />
-      </Button>
+      <Tooltip title="Descargar consulta en PDF">
+        <Button onClick={() => downloadPdf(url)}>
+          <FontAwesomeIcon icon={faFilePdf} size="2x" />
+        </Button>
+      </Tooltip>
     </div>
   )
 }
--- src/pages/Supplies/SuppliesWithLowStock.tsx
+++ src/pages/Supplies/SuppliesWithLowStock.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import GenericTable from '../../components/GenericTable';
 import { Column } from '../../components/GenericTable';
 import { SuppliesResponse, TransformedSupplies, Batch } from '../../interfaces/Supplies';
@@ -132,8 +134,11 @@ export default function SuppliesWithLowStock() {
   };
 
   useEffect(() => {
-    getSupplies();
-    getProviders();
+    toast.promise(Promise.all([getSupplies(), getProviders()]), {
+      pending: 'Cargando insumos...',
+      success: 'Insumos cargados',
+      error: 'Error al cargar insumos',
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -160,6 +165,7 @@ export default function SuppliesWithLowStock() {
         providers={providers}
         productsByProvider={productsByProvider}
       />
+      <ToastContainer />
     </div>
   );
 }
