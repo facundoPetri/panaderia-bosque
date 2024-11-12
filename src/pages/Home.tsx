cf6ec86f257a567e0cd2b9af6972b4f3b0c347c3
Franco <francoandes12@gmail.com>
2024-11-12 20:39:26 -0300

    cambios home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,6 +14,9 @@ import {
   Input,
   MenuItem,
   TextField,
+  Card,
+  CardContent,
+  CardActions,
 } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
@@ -23,6 +26,7 @@ import { SuppliesResponse, SupplyUsage } from '../interfaces/Supplies'
 import { request, requestToast } from '../common/request'
 import { formatISODateString } from '../utils/dateUtils'
 import { getSupplyUnit } from '../utils/getSupplyUnit'
+import { RecipesResponse } from '../interfaces/Recipes'
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -36,7 +40,7 @@ const useStyles = makeStyles((theme) => {
       marginTop: '1rem',
       width: '50%',
       marginBottom: '1rem',
-      paddingTop:'0px',
+      paddingTop: '0px',
       border: `1px solid ${theme.palette.primary.main}`,
       '& li': {
         '&:hover': {
@@ -46,38 +50,66 @@ const useStyles = makeStyles((theme) => {
     },
     title: {
       textAlign: 'center',
-      ':last-child': {
-        marginTop: '1rem',
-      },
+    },
+    suppliesWrapper: {
+      marginTop: '1rem',
     },
     container: {
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'center',
       marginTop: '1rem',
     },
     button: {
       marginLeft: '1rem',
     },
-    header:{
-      backgroundColor:theme.palette.primary.main,
+    header: {
+      backgroundColor: theme.palette.primary.main,
     },
-    listItem:{
-      width:'33%'
-    }
+    listItem: {
+      width: '33.3%',
+    },
+    headerWrapper: {
+      display: 'flex',
+      alignItems: 'center',
+
+      marginLeft: '30%',
+    },
+    bottomWrapper: {
+      flexDirection: 'column',
+    },
+    lastSupply: {
+      width: '500px',
+    },
+    cardWrappers: {
+      display: 'flex',
+      marginTop: '1rem',
+    },
+    card: {
+      width: '200px',
+      margin: '0px 1rem',
+    },
   }
 })
-
+interface SupplyUsageToSend {
+  supply: string
+  quantity: number
+  date_used: string
+}
+const filterDaysOptions = [
+  { days: 1, label: 'hoy' },
+  { days: 7, label: 'esta semana' },
+]
 function Home() {
   const classes = useStyles()
   const [openDialog, setOpenDialog] = useState(false)
   const [supplies, setSupplies] = useState<SuppliesResponse[]>([])
   const [suppliesLog, setSuppliesLog] = useState<SupplyUsage[]>([])
-  const [supplyUsage, setSupplyUsage] = useState({
-    supply: '',
-    quantity: 0,
-    date_used: '',
-  })
+  const [recipes, setRecipes] = useState<RecipesResponse[]>([])
+  const [filterDays, setFilterDays] = useState(1)
+  const [selectedRecipe, setSelectedRecipe] = useState<RecipesResponse | null>(
+    null
+  )
+  const [supplyUsage, setSupplyUsage] = useState<SupplyUsageToSend[]>([])
   const getSupplies = async () => {
     try {
       const res = await request<SuppliesResponse[]>({
@@ -94,7 +126,7 @@ function Home() {
   const getSuppliesUsageLog = async () => {
     try {
       const res = await request<SupplyUsage[]>({
-        path: '/supplies-usage',
+        path: `/supplies-usage?filterDays=${filterDays}`,
         method: 'GET',
       })
       if (res) {
@@ -112,11 +144,7 @@ function Home() {
   }
   const handleCancel = () => {
     handleCloseDialog()
-    setSupplyUsage({
-      supply: '',
-      quantity: 0,
-      date_used: '',
-    })
+    setSupplyUsage([])
   }
   const handleConfirm = async () => {
     try {
@@ -136,21 +164,72 @@ function Home() {
     }
     handleCancel()
   }
-
+  const getRecipes = async () => {
+    try {
+      const res = await request<RecipesResponse[]>({
+        path: '/recipes',
+        method: 'GET',
+      })
+      if (res) {
+        setRecipes(res)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
   useEffect(() => {
     getSupplies()
-    getSuppliesUsageLog()
+    getRecipes()
   }, [])
+  useEffect(() => {
+    getSuppliesUsageLog()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filterDays])
+  const handleChangeQuantity = (value: string, supplyId: string) => {
+    setSupplyUsage((prev) => {
+      const existingSupply = prev.find((item) => item.supply === supplyId)
 
+      if (existingSupply) {
+        return prev.map((item) =>
+          item.supply === supplyId ? { ...item, quantity: Number(value) } : item
+        )
+      } else {
+        return [
+          ...prev,
+          {
+            supply: supplyId,
+            quantity: Number(value),
+            date_used: new Date().toISOString(),
+          },
+        ]
+      }
+    })
+  }
   return (
     <div>
       <Typography className={classes.title}>Datos mas relevantes</Typography>
       <div className={classes.wrapper}>
         <div className="section1">
           <Box className={classes.container}>
-            <Typography className={classes.title}>
-              Ultimos insumos usados esta semana
-            </Typography>
+            <Box className={classes.headerWrapper}>
+              <Typography className={classes.lastSupply}>
+                Ultimos insumos usados
+              </Typography>
+              <Select
+                fullWidth
+                labelId="recetasSelector"
+                name="recipes"
+                value={filterDays}
+                onChange={(e) => setFilterDays(Number(e.target.value))}
+                input={<Input />}
+              >
+                {filterDaysOptions.map((filter) => (
+                  <MenuItem key={filter.days} value={filter.days}>
+                    {filter.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Box>
             <Button
               variant="contained"
               color="primary"
@@ -172,14 +251,18 @@ function Home() {
               const labelId = `checkbox-list-label-${value}`
               return (
                 <ListItem key={value._id} dense>
-                  <ListItemText className={classes.listItem} id={labelId} primary={value.supply.name} />{' '}
                   <ListItemText
-                  className={classes.listItem}
+                    className={classes.listItem}
+                    id={labelId}
+                    primary={value.supply.name}
+                  />{' '}
+                  <ListItemText
+                    className={classes.listItem}
                     id={labelId}
                     primary={formatISODateString(value.date_used)}
                   />
                   <ListItemText
-                  className={classes.listItem}
+                    className={classes.listItem}
                     id={labelId}
                     primary={`${value.quantity} ${getSupplyUnit(
                       value.supply._id,
@@ -190,6 +273,57 @@ function Home() {
               )
             })}
           </List>
+          <Box className={classes.container}>
+            <Box
+              className={`${classes.headerWrapper} ${classes.bottomWrapper}`}
+            >
+              <Typography>Pedidos a proveedores pendientes</Typography>
+              <Box className={classes.cardWrappers}>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography gutterBottom>10/11/2024</Typography>
+                    <Typography variant="h5" component="div">
+                      Pedido 1
+                    </Typography>
+                    <Typography>Bimbo</Typography>
+                    <Typography variant="body2">Pan de hamburguesa</Typography>
+                    <Typography variant="body2">Pan Lactal</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Ver pedido</Button>
+                  </CardActions>
+                </Card>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography gutterBottom>10/11/2024</Typography>
+                    <Typography variant="h5" component="div">
+                      Pedido 1
+                    </Typography>
+                    <Typography>Bimbo</Typography>
+                    <Typography variant="body2">Pan de hamburguesa</Typography>
+                    <Typography variant="body2">Pan Lactal</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Ver pedido</Button>
+                  </CardActions>
+                </Card>{' '}
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography gutterBottom>10/11/2024</Typography>
+                    <Typography variant="h5" component="div">
+                      Pedido 1
+                    </Typography>
+                    <Typography>Bimbo</Typography>
+                    <Typography variant="body2">Pan de hamburguesa</Typography>
+                    <Typography variant="body2">Pan Lactal</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Ver pedido</Button>
+                  </CardActions>
+                </Card>
+              </Box>
+            </Box>
+          </Box>
         </div>
       </div>
       <GenericDialog
@@ -199,62 +333,50 @@ function Home() {
         content={
           <div>
             <FormControl fullWidth>
-              <InputLabel id="insumosSelector">Insumos</InputLabel>
+              <InputLabel id="recetasSelector">Recetas</InputLabel>
               <Select
-                labelId="insumosSelector"
-                //className={classes.select}
-                name="supplies"
-                value={supplyUsage.supply}
+                labelId="recetasSelector"
+                name="recipes"
+                value={filterDays}
                 onChange={(e) =>
-                  setSupplyUsage((prev) => {
-                    return {
-                      ...prev,
-                      supply: e.target.value as string,
-                    }
-                  })
+                  setSelectedRecipe(
+                    recipes.find((r) => r._id === (e.target.value as string)) ||
+                      null
+                  )
                 }
                 label="Insumos"
                 input={<Input />}
                 fullWidth
               >
-                {supplies.map((sup) => (
-                  <MenuItem key={sup._id} value={sup._id}>
-                    {sup.name}
+                {recipes.map((recipe) => (
+                  <MenuItem key={recipe._id} value={recipe._id}>
+                    {recipe.name}
                   </MenuItem>
                 ))}
               </Select>
             </FormControl>
-            <TextField
-              margin="dense"
-              label="Cantidad"
-              type="number"
-              fullWidth
-              value={supplyUsage.quantity}
-              onChange={(e) =>
-                setSupplyUsage((prev) => {
-                  return {
-                    ...prev,
-                    quantity: Number(e.target.value),
-                  }
-                })
-              }
-            />
-            <TextField
-              margin="dense"
-              label="Fecha de uso"
-              type="date"
-              fullWidth
-              InputLabelProps={{ shrink: true }}
-              value={supplyUsage.date_used}
-              onChange={(e) =>
-                setSupplyUsage((prev) => {
-                  return {
-                    ...prev,
-                    date_used: e.target.value,
-                  }
-                })
-              }
-            />
+            {selectedRecipe && (
+              <>
+                {selectedRecipe.supplies.map((supply) => (
+                  <Box className={classes.suppliesWrapper}>
+                    <Typography>{`${supply.name} (${supply.unit})`}</Typography>
+                    <TextField
+                      margin="dense"
+                      label="Cantidad"
+                      type="number"
+                      fullWidth
+                      value={
+                        supplyUsage.find((item) => item.supply === supply._id)
+                          ?.quantity || 0
+                      }
+                      onChange={(e) =>
+                        handleChangeQuantity(e.target.value, supply._id)
+                      }
+                    />
+                  </Box>
+                ))}
+              </>
+            )}
           </div>
         }
         primaryButtonAction={handleConfirm}
