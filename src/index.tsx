7616edd87c85d435093cc95ec18aa216b43831e3
NicoGRodriguez <nicolas_rodriguez_94@mi.unc.edu.ar>
2024-11-20 21:07:24 -0300

    #59 nueva modal creacion calidad y puntualidad

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Main from './Main'
+import React from 'react';
+import { render } from 'react-dom';
+import Main from './Main';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns'; // Librería de manejo de fechas
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
+
 render(
   <React.StrictMode>
-    <Main />
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Main />
+    </MuiPickersUtilsProvider>
   </React.StrictMode>,
   root
-)
+);
